2362ee85bccc62dbf9637616d437f450dafdfe1e
Ally-Matias <allyquison.matias@gmail.com>
2025-05-26 21:30:12 -0300

    feat: add new version

--- assets/js/toggleVisibility.js
+++
@@ -1,6 +0,0 @@
-// altera visibilidade de um elemento
-function toggleVisibility(element, classes) {
-    scrollY === 0 ? element.classList.remove(classes) : element.classList.add(classes);
-}
-
-export { toggleVisibility };
